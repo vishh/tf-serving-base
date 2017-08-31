b651fc3330b24f2c1098a5763824ad5fab629fe5
Vishnu kannan <vishnuk@google.com>
2017-08-31 16:34:00 -0700

    Fix GCR push issue
    Signed-off-by: Vishnu kannan <vishnuk@google.com>

--- build_and_export.sh
+++ build_and_export.sh
@@ -2,6 +2,7 @@
 set -x
 project=$1
 docker-credential-gcr gcr-login
+docker-credential-gcr config --token-source="store"
 PATH=/usr/bin:$PATH bazel build --define project=${project} :all
 PATH=/usr/bin:$PATH bazel run --define project=${project} :push
 docker-credential-gcr gcr-logout
